f5a113ba0091c6ee4b9c0be7579c6757bebfcc67
jesus1080 <jesusruiz@unicauca.edu.co>
2022-02-06 20:09:12 -0500

    feat: function get image atribute, name

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
@@ -21,8 +22,35 @@ class Product extends Model
         'image',
         'description',
         'stock',
-        'color',
-        'size',
+        'status',
     ];
+
+    public function getImageAttribute(){
+        
+        $path = explode("public/", $this->attributes['image']);
+        return url('/'.$path[1]);
+    }
+
+    public function getImageName(){
+        $path = explode("/", $this->attributes['image']);
+        
+        return $path[3];
+    }
+
+    public function scopeName(Builder $query, ? string $name): Builder
+    {
+        if (null !== $name) {
+            return $this->searchByField($query, 'name', "%$name%", 'like');
+        }
+
+        return $query;
+    }
+
+    private function searchByField(Builder $query, string $field, string $value, string $operator = null): Builder
+    {
+        return $query->where($field, $operator, $value);
+    }
+    
+
 
 }
