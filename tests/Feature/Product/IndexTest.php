f30788ab4bdec7550dcb2dbd281a57053f2f9c25
jesus1080 <jesusruiz@unicauca.edu.co>
2022-02-13 19:28:05 -0500

    fix: fixed product tests

--- tests/Feature/Product/IndexTest.php
+++ tests/Feature/Product/IndexTest.php
@@ -35,5 +35,26 @@ class IndexTest extends TestCase
             );
             $response->assertStatus(200);
     }
+
+    /** @test */
+    public function testaUserAdminAutenticateCanListProductsWithActions(): void{
+            
+      $this->withoutExceptionHandling();
+      Role::create(['name' => 'admin']);
+      $user = User::factory()->create()->assignRole('admin');
+      $this->actingAs($user);
+      $products = Product::factory(10)->create();
+      $response = $this->get(route('products.store'));
+      $response->assertInertia(fn (Assert $page) => $page
+        ->component('Product/Products')
+        ->has('products',fn (Assert $page) => $page
+        ->where('total',10)
+        ->where('last_page',3)
+        ->has('data', 4)
+        ->etc()
+        )
+      );
+      $response->assertStatus(200);
+}
     
 }
